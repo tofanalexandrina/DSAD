5db42151619599f1e83812bb04b36641ac5c76ee
alexandrina <146881298+tofanalexandrina@users.noreply.github.com>
2025-06-26 13:40:33 +0300

    Update s2-restanta2024.py

--- s2-restanta2024/s2-restanta2024.py
+++ s2-restanta2024/s2-restanta2024.py
@@ -13,12 +13,12 @@ df_nou=df_nou.sort_values(by="Internet", ascending=False)
 df_nou.to_csv("Cerinta1.csv", index=False)
 
 #2
-merged=pd.merge(df_netflix, df_coduri, on="Cod")
-indicatori=['Librarie','CostLunarBasic','CostLunarStandard','CostLunarPremium','Internet','HDI','Venit','IndiceFericire','IndiceEducatie']
-grupate=merged.groupby("Continent")[indicatori].agg(['mean', 'std'])
-grupate=(grupate.xs('std', axis=1, level=1) / grupate.xs('mean', axis=1, level=1)).round(3).reset_index()
-grupate=grupate.sort_values(by="Librarie", ascending=False)
-grupate.to_csv("Cerinta2.csv", index=False)
+merged=pd.merge(df_coduri, df_netflix, on='Cod')
+merged=merged.groupby("Continent")[coloane].agg(['mean', 'std'])
+merged=merged.xs('std', axis=1, level=1)/merged.xs('mean', axis=1, level=1)
+merged=merged.round(3).reset_index()
+merged=merged.sort_values(by="Librarie", ascending=False)
+merged.to_csv("Cerinta2.csv", index=False)
 
 #3
 #--ACP
